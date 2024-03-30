8e3205b261f2982b0e95a12defa79e1aebb1c666
haricharan9120 <haricharan9120@gmail.com>
2024-03-30 16:42:41 +0530

    first commit

---
+++ food/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from food.views import *
+app_name='something'
+urlpatterns=[
+    path('biriyani/',biriyani,name='biriyani'),
+    path('haleem/',haleem,name='haleem'),
+]
